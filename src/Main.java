088c08999cbf4183953f86eccb40000b74842319
Vale-n-tina <valentia.grg@gmail.com>
2025-01-07 13:01:46 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.print("Hello and welcome!");
+
+        int x=0;
+        System.out.println(x);
+
+        String primaParola="ciao";
+        String secondaParola="Valentina";
+        String terzaParola= primaParola + " " + secondaParola;
+        System.out.print(terzaParola);
+
+    }
+}
